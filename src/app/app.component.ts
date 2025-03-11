77c26f3091afd58f0511c13d63d3a7349bd42458
Himanshu Arya <arya.h.ntl@gmail.com>
2025-03-12 00:38:42 +0530

    learning data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learningNg';
+  username:String="";
+  imageURL:string="assets/hanuman.jpg";
   welcomeMessage:String = 'Welcome to Angular 8';
   
+  ChangeMessage(){
+    this.welcomeMessage='Kese ho app';
+  }
+
+  ResetMessage(){
+    this.welcomeMessage='Welcome to Angular 8';
+  }
+
   public WelcomeMessage():any{
     return 'Dear User ' +this.welcomeMessage;
 
+  }
+  ButtonTestSize:number=20;
+  applyBold: String='bold';
+
+  applyStyle(){
+    var myStyles={
+      'font-size.px':this.ButtonTestSize,
+      'font-weight':this.applyBold,
+      'color':'red'
+
+    }
+    return myStyles;
+  }
+  count:number=0;
+
+  increaseCount(){
+    this.count++;
+  }
+  decreaseCount(){
+    this.count--;
   }
 }
